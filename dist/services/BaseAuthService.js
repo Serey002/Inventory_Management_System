05d35e6c2a07d6679cf9b08f46faf31349842de1
Serey002 <serey.phem.12022006@gmail.com>
2026-05-24 16:20:47 +0700

    Add project files and update .gitignore

--- dist/services/BaseAuthService.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BaseAuthService = void 0;
-class BaseAuthService {
-}
-exports.BaseAuthService = BaseAuthService;
-exports.default = BaseAuthService;
